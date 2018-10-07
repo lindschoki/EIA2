fbf765673305247ee87c6670edbf3adf2e0cd462
lindschoki <43847385+lindschoki@users.noreply.github.com>
2018-10-07 14:08:13 +0200

    Add files via upload

---
+++ Aufgabe 0/Aufgabe0.js
@@ -0,0 +1,18 @@
+/**
+Aufgabe: 0
+Name: Arno Lindner
+Matrikel: 259153
+Datum: 07.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+**/
+function Feldeingabe() {
+    var Name = prompt("Gib deinen Namen hier ein");
+    var node = document.getElementById("innerhtmlcontent");
+    node.innerHTML += "Servus ";
+    node.innerHTML += Name;
+    node.innerHTML += " wie geht's?";
+    console.log("Servus " + Name + "wie geht's?");
+}
+document.addEventListener('DOMContentLoaded', Feldeingabe);
+//# sourceMappingURL=Aufgabe0.js.map
