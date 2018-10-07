534bb5c2846f33154a12c9ed7c0d7a1ea320214e
Arno <Arno@X-Ray>
2018-10-07 13:57:22 +0200

    ss

--- Aufgabe 0/Aufgabe0.js
+++ Aufgabe 0/Aufgabe0.js
@@ -2,13 +2,13 @@
 Aufgabe: 0
 Name: Arno Lindner
 Matrikel: 259153
-Datum: 06.10.2018
+Datum: 07.10.2018
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
 function Feldeingabe() {
     var Name = prompt("Gib deinen Namen hier ein");
-    var node = document.getElementById("htmlcontent");
+    var node = document.getElementById("innerhtmlcontent");
     node.innerHTML += "Servus ";
     node.innerHTML += Name;
     node.innerHTML += " wie geht's?";
