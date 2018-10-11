ef1d892c63baee35bd2cae77ca834af04479dc5c
Arno <Arno@X-Ray>
2018-10-11 13:39:00 +0200

    Verbesserter Code

--- Aufgabe 0/Aufgabe0.js
+++ Aufgabe 0/Aufgabe0.js
@@ -7,12 +7,12 @@ Datum: 07.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
 function Feldeingabe() {
-    var Name = prompt("Gib deinen Namen hier ein");
+    var NAME = prompt("Gib deinen Namen hier ein");
     var node = document.getElementById("innerhtmlcontent");
     node.innerHTML += "Servus ";
-    node.innerHTML += Name;
+    node.innerHTML += NAME;
     node.innerHTML += " wie geht's?";
-    console.log("Servus " + Name + "wie geht's?");
+    console.log("Servus " + NAME + "wie geht's?");
 }
-document.addEventListener('DOMContentLoaded', Feldeingabe);
+document.addEventListener("DOMContentLoaded", Feldeingabe);
 //# sourceMappingURL=Aufgabe0.js.map
--- Aufgabe 0/Aufgabe0.ts
+++ Aufgabe 0/Aufgabe0.ts
@@ -7,13 +7,13 @@ Datum: 07.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
     
-function Feldeingabe() {
-        var Name : string = prompt("Gib deinen Namen hier ein");
-        var node = document.getElementById("innerhtmlcontent");
+function Feldeingabe(): void {
+        var NAME: string = prompt("Gib deinen Namen hier ein");
+        var node: HTMLElement = document.getElementById("innerhtmlcontent");
         node.innerHTML += "Servus ";
-        node.innerHTML += Name;
+        node.innerHTML += NAME;
         node.innerHTML += " wie geht's?";
-        console.log("Servus "+ Name + "wie geht's?");
+        console.log("Servus " + NAME + "wie geht's?");
     }
-    document.addEventListener('DOMContentLoaded', Feldeingabe);
+    document.addEventListener("DOMContentLoaded", Feldeingabe);
   
