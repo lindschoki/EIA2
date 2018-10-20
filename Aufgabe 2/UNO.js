f834cb5a04bd42fb286edcfbd12d0fa214eaec0e
Arno <Arno@X-Ray>
2018-10-20 22:52:15 +0200

    erster code

---
+++ Aufgabe 2/UNO.js
@@ -0,0 +1,448 @@
+var UNO;
+(function (UNO) {
+    let C1 = {
+        color: "#ff0000",
+        kartenwert: "0",
+    };
+    let C2 = {
+        color: "#ff0000",
+        kartenwert: "1",
+    };
+    let C3 = {
+        color: "#ff0000",
+        kartenwert: "1",
+    };
+    let C4 = {
+        color: "#ff0000",
+        kartenwert: "2",
+    };
+    let C5 = {
+        color: "#ff0000",
+        kartenwert: "2",
+    };
+    let C6 = {
+        color: "#ff0000",
+        kartenwert: "3",
+    };
+    let C7 = {
+        color: "#ff0000",
+        kartenwert: "3",
+    };
+    let C8 = {
+        color: "#ff0000",
+        kartenwert: "4",
+    };
+    let C9 = {
+        color: "#ff0000",
+        kartenwert: "4",
+    };
+    let C10 = {
+        color: "#ff0000",
+        kartenwert: "5",
+    };
+    let C11 = {
+        color: "#ff0000",
+        kartenwert: "5",
+    };
+    let C12 = {
+        color: "#ff0000",
+        kartenwert: "6",
+    };
+    let C13 = {
+        color: "#ff0000",
+        kartenwert: "6",
+    };
+    let C14 = {
+        color: "#ff0000",
+        kartenwert: "7",
+    };
+    let C15 = {
+        color: "#ff0000",
+        kartenwert: "7",
+    };
+    let C16 = {
+        color: "#ff0000",
+        kartenwert: "8",
+    };
+    let C17 = {
+        color: "#ff0000",
+        kartenwert: "8",
+    };
+    let C18 = {
+        color: "#ff0000",
+        kartenwert: "9",
+    };
+    let C19 = {
+        color: "#ff0000",
+        kartenwert: "9",
+    };
+    let C20 = {
+        color: "#0000ff",
+        kartenwert: "0",
+    };
+    let C21 = {
+        color: "#0000ff",
+        kartenwert: "1",
+    };
+    let C22 = {
+        color: "#0000ff",
+        kartenwert: "1",
+    };
+    let C23 = {
+        color: "#0000ff",
+        kartenwert: "2",
+    };
+    let C24 = {
+        color: "#0000ff",
+        kartenwert: "2",
+    };
+    let C25 = {
+        color: "#0000ff",
+        kartenwert: "3",
+    };
+    let C26 = {
+        color: "#0000ff",
+        kartenwert: "3",
+    };
+    let C27 = {
+        color: "#0000ff",
+        kartenwert: "4",
+    };
+    let C28 = {
+        color: "#0000ff",
+        kartenwert: "4",
+    };
+    let C29 = {
+        color: "#0000ff",
+        kartenwert: "5",
+    };
+    let C30 = {
+        color: "#0000ff",
+        kartenwert: "5",
+    };
+    let C31 = {
+        color: "#0000ff",
+        kartenwert: "6",
+    };
+    let C32 = {
+        color: "#0000ff",
+        kartenwert: "6",
+    };
+    let C33 = {
+        color: "#0000ff",
+        kartenwert: "7",
+    };
+    let C34 = {
+        color: "#0000ff",
+        kartenwert: "7",
+    };
+    let C35 = {
+        color: "#0000ff",
+        kartenwert: "8",
+    };
+    let C36 = {
+        color: "#0000ff",
+        kartenwert: "8",
+    };
+    let C37 = {
+        color: "#0000ff",
+        kartenwert: "9",
+    };
+    let C38 = {
+        color: "#0000ff",
+        kartenwert: "9",
+    };
+    let C39 = {
+        color: "#00ff00",
+        kartenwert: "0",
+    };
+    let C40 = {
+        color: "#00ff00",
+        kartenwert: "1",
+    };
+    let C41 = {
+        color: "#00ff00",
+        kartenwert: "1",
+    };
+    let C42 = {
+        color: "#00ff00",
+        kartenwert: "2",
+    };
+    let C43 = {
+        color: "#00ff00",
+        kartenwert: "2",
+    };
+    let C44 = {
+        color: "#00ff00",
+        kartenwert: "3",
+    };
+    let C45 = {
+        color: "#00ff00",
+        kartenwert: "3",
+    };
+    let C46 = {
+        color: "#00ff00",
+        kartenwert: "4",
+    };
+    let C47 = {
+        color: "#00ff00",
+        kartenwert: "4",
+    };
+    let C48 = {
+        color: "#00ff00",
+        kartenwert: "5",
+    };
+    let C49 = {
+        color: "#00ff00",
+        kartenwert: "5",
+    };
+    let C50 = {
+        color: "#00ff00",
+        kartenwert: "6",
+    };
+    let C51 = {
+        color: "#00ff00",
+        kartenwert: "6",
+    };
+    let C52 = {
+        color: "#00ff00",
+        kartenwert: "7",
+    };
+    let C53 = {
+        color: "#00ff00",
+        kartenwert: "7",
+    };
+    let C54 = {
+        color: "#00ff00",
+        kartenwert: "8",
+    };
+    let C55 = {
+        color: "#00ff00",
+        kartenwert: "8",
+    };
+    let C56 = {
+        color: "#00ff00",
+        kartenwert: "9",
+    };
+    let C57 = {
+        color: "#00ff00",
+        kartenwert: "9",
+    };
+    let C58 = {
+        color: "#ffff00",
+        kartenwert: "0",
+    };
+    let C59 = {
+        color: "#ffff00",
+        kartenwert: "1",
+    };
+    let C60 = {
+        color: "#ffff00",
+        kartenwert: "1",
+    };
+    let C61 = {
+        color: "#ffff00",
+        kartenwert: "2",
+    };
+    let C62 = {
+        color: "#ffff00",
+        kartenwert: "2",
+    };
+    let C63 = {
+        color: "#ffff00",
+        kartenwert: "3",
+    };
+    let C64 = {
+        color: "#ffff00",
+        kartenwert: "3",
+    };
+    let C65 = {
+        color: "#ffff00",
+        kartenwert: "4",
+    };
+    let C66 = {
+        color: "#ffff00",
+        kartenwert: "4",
+    };
+    let C67 = {
+        color: "#ffff00",
+        kartenwert: "5",
+    };
+    let C68 = {
+        color: "#ffff00",
+        kartenwert: "5",
+    };
+    let C69 = {
+        color: "#ffff00",
+        kartenwert: "6",
+    };
+    let C70 = {
+        color: "#ffff00",
+        kartenwert: "6",
+    };
+    let C71 = {
+        color: "#ffff00",
+        kartenwert: "7",
+    };
+    let C72 = {
+        color: "#ffff00",
+        kartenwert: "7",
+    };
+    let C73 = {
+        color: "#ffff00",
+        kartenwert: "8",
+    };
+    let C74 = {
+        color: "#ffff00",
+        kartenwert: "8",
+    };
+    let C75 = {
+        color: "#ffff00",
+        kartenwert: "9",
+    };
+    let C76 = {
+        color: "#ffff00",
+        kartenwert: "9",
+    };
+    let C77 = {
+        color: "black",
+        kartenwert: "+4",
+    };
+    let C78 = {
+        color: "#black",
+        kartenwert: "+4",
+    };
+    let C79 = {
+        color: "#ff0000",
+        kartenwert: "Swap",
+    };
+    let C80 = {
+        color: "#ff0000",
+        kartenwert: "Swap",
+    };
+    let C81 = {
+        color: "#ff0000",
+        kartenwert: "Stop",
+    };
+    let C82 = {
+        color: "#ff0000",
+        kartenwert: "Stop",
+    };
+    let C83 = {
+        color: "#ff0000",
+        kartenwert: "+2",
+    };
+    let C84 = {
+        color: "#ff0000",
+        kartenwert: "+2",
+    };
+    let C85 = {
+        color: "#0000ff",
+        kartenwert: "Swap",
+    };
+    let C86 = {
+        color: "#0000ff",
+        kartenwert: "Swap",
+    };
+    let C87 = {
+        color: "#0000ff",
+        kartenwert: "Stop",
+    };
+    let C88 = {
+        color: "#0000ff",
+        kartenwert: "Stop",
+    };
+    let C89 = {
+        color: "#0000ff",
+        kartenwert: "+2",
+    };
+    let C90 = {
+        color: "#0000ff",
+        kartenwert: "+2",
+    };
+    let C91 = {
+        color: "#00ff00",
+        kartenwert: "Swap",
+    };
+    let C92 = {
+        color: "#00ff00",
+        kartenwert: "Swap",
+    };
+    let C93 = {
+        color: "#00ff00",
+        kartenwert: "Stop",
+    };
+    let C94 = {
+        color: "#00ff00",
+        kartenwert: "Stop",
+    };
+    let C95 = {
+        color: "#00ff00",
+        kartenwert: "+2",
+    };
+    let C96 = {
+        color: "#00ff00",
+        kartenwert: "+2",
+    };
+    let C97 = {
+        color: "#ffff00",
+        kartenwert: "Swap",
+    };
+    let C98 = {
+        color: "#ffff00",
+        kartenwert: "Swap",
+    };
+    let C99 = {
+        color: "#ffff00",
+        kartenwert: "Stop",
+    };
+    let C100 = {
+        color: "#ffff00",
+        kartenwert: "Stop",
+    };
+    let C101 = {
+        color: "#ffff00",
+        kartenwert: "+2",
+    };
+    let C102 = {
+        color: "#ffff00",
+        kartenwert: "+2",
+    };
+    let C103 = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+    };
+    let C104 = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+    };
+    let C105 = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+    };
+    let C106 = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+    };
+    let C107 = {
+        color: "#black",
+        kartenwert: "+4",
+    };
+    let C108 = {
+        color: "#black",
+        kartenwert: "+4",
+    };
+    let Cards = [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16, C17,
+        C18, C19, C20, C21, C22, C23, C24, C25, C26, C27, C28, C29, C30, C31, C32, C33,
+        C34, C35, C36, C37, C38, C39, C40, C41, C42, C43, C44, C45, C46, C47, C48, C49,
+        C50, C51, C52, C53, C54, C55, C56, C57, C58, C59, C60, C61, C62, C63, C64, C65,
+        C66, C67, C68, C69, C70, C71, C72, C73, C74, C75, C76, C77, C78, C79, C80, C81,
+        C82, C83, C84, C85, C86, C87, C88, C89, C90, C91, C92, C93, C94, C95, C96, C97,
+        C98, C99, C100, C101, C102, C103, C104, C105, C106, C107, C108];
+    let ZIEHEN = prompt("Gib deine Kartenanzahl ein");
+    let x;
+    for (let i = 0; i < x; i++) {
+        let randomCards = Cards[Math.floor(Cards.length * Math.random())];
+    }
+})(UNO || (UNO = {}));
+//# sourceMappingURL=UNO.js.map
---
+++ Aufgabe 2/UNO.ts
@@ -0,0 +1,571 @@
+namespace UNO {
+    
+    interface UNOKarten {
+        color: string;
+        kartenwert: string;
+        }
+    
+    
+    let C1: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "0",
+        }
+    
+    let C2: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "1",
+        }
+    
+    let C3: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "1",
+        }
+    
+    let C4: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "2",
+        }
+    
+    let C5: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "2",
+        }
+    
+    let C6: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "3",
+        }
+    
+    let C7: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "3",
+        }
+    
+    let C8: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "4",
+        }
+    
+    let C9: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "4",
+        }
+    
+    let C10: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "5",
+        }
+    
+    let C11: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "5",
+        }
+    
+    let C12: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "6",
+        }
+    
+    let C13: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "6",
+        }
+    
+    let C14: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "7",
+        }
+    
+    let C15: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "7",
+        }
+    
+    let C16: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "8",
+        }
+    
+    let C17: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "8",
+        }
+    
+    let C18: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "9",
+        }
+    
+    let C19: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "9",
+        }
+    
+    let C20: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "0",
+        }
+    
+    let C21: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "1",
+        }
+    let C22: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "1",
+        }
+    
+    let C23: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "2",
+        }
+    
+    let C24: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "2",
+        }
+    
+    let C25: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "3",
+        }
+    
+    let C26: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "3",
+        }
+    
+    let C27: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "4",
+        }
+    
+    let C28: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "4",
+        }
+    
+    let C29: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "5",
+        }
+    
+    let C30: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "5",
+        }
+    
+    let C31: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "6",
+        }
+    let C32: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "6",
+        }
+    
+    let C33: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "7",
+        }
+    
+    let C34: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "7",
+        }
+    
+    let C35: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "8",
+        }
+    
+    let C36: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "8",
+        }
+    
+    let C37: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "9",
+        }
+    
+    let C38: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "9",
+        }
+    
+    let C39: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "0",
+        }
+    
+    let C40: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "1",
+        }
+    
+    let C41: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "1",
+        }
+    
+    let C42: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "2",
+        }
+    
+    let C43: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "2",
+        }
+    
+    let C44: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "3",
+        }
+    
+    let C45: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "3",
+        }
+    
+    let C46: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "4",
+        }
+    
+    let C47: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "4",
+        }
+    
+    let C48: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "5",
+        }
+    
+    let C49: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "5",
+        }
+    
+    let C50: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "6",
+        }
+    
+    let C51: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "6",
+        }
+   
+    let C52: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "7",
+        }
+    
+    let C53: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "7",
+        }
+    
+    let C54: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "8",
+        }
+    
+    let C55: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "8",
+        }
+    
+     let C56: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "9",
+        }
+    
+    let C57: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "9",
+        }
+    
+    let C58: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "0",
+        }
+    
+    let C59: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "1",
+        }
+    
+    let C60: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "1",
+        }
+    
+   let C61: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "2",
+        }
+    
+    let C62: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "2",
+        }
+    
+    let C63: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "3",
+        }
+    
+    let C64: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "3",
+        }
+    
+    let C65: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "4",
+        }
+    
+    let C66: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "4",
+        }
+    
+    let C67: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "5",
+        }
+    
+    let C68: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "5",
+        }
+    
+    let C69: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "6",
+        }
+    
+    let C70: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "6",
+        }
+    
+    let C71: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "7",
+        }
+    
+    let C72: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "7",
+        }
+    
+    let C73: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "8",
+        }
+    
+    let C74: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "8",
+        }
+    
+    let C75: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "9",
+        }
+    
+    let C76: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "9",
+        }
+  
+    let C77: UNOKarten = {
+        color: "black",
+        kartenwert: "+4",
+        }
+    
+    let C78: UNOKarten = {
+        color: "#black",
+        kartenwert: "+4",
+        }
+    
+    let C79: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "Swap",
+        }
+    
+    let C80: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "Swap",
+        }
+    
+    let C81: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "Stop",
+        }
+    
+    let C82: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "Stop",
+        }
+    
+    let C83: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "+2",
+        }
+    
+    let C84: UNOKarten = {
+        color: "#ff0000",
+        kartenwert: "+2",
+        }
+    
+    let C85: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "Swap",
+        }
+    
+    let C86: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "Swap",
+        }
+    
+    let C87: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "Stop",
+        }
+    
+    let C88: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "Stop",
+        }
+    
+    let C89: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "+2",
+        }
+    
+    let C90: UNOKarten = {
+        color: "#0000ff",
+        kartenwert: "+2",
+        }
+    
+    let C91: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "Swap",
+        }
+    
+    let C92: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "Swap",
+        }
+    
+    let C93: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "Stop",
+        }
+    
+    let C94: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "Stop",
+        }
+    
+    let C95: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "+2",
+        }
+    
+    let C96: UNOKarten = {
+        color: "#00ff00",
+        kartenwert: "+2",
+        }
+    
+    let C97: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "Swap",
+        }
+    
+    let C98: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "Swap",
+        }
+    
+    let C99: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "Stop",
+        }
+    
+    let C100: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "Stop",
+        }
+    
+    let C101: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "+2",
+        }
+    
+    let C102: UNOKarten = {
+        color: "#ffff00",
+        kartenwert: "+2",
+        }
+    
+    let C103: UNOKarten = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+        }
+    
+    let C104: UNOKarten = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+        }
+    
+    let C105: UNOKarten = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+        }
+    
+    let C106: UNOKarten = {
+        color: "#black",
+        kartenwert: "Wishcolor",
+        }
+    
+    let C107: UNOKarten = {
+        color: "#black",
+        kartenwert: "+4",
+        }
+    
+    let C108: UNOKarten = {
+        color: "#black",
+        kartenwert: "+4",
+        }
+   
+    let Cards: UNOKarten[] = [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16, C17,
+                                     C18, C19, C20, C21, C22, C23, C24, C25, C26, C27, C28, C29, C30, C31, C32, C33, 
+                                     C34, C35, C36, C37, C38, C39, C40, C41, C42, C43, C44, C45, C46, C47, C48, C49, 
+                                     C50, C51, C52, C53, C54, C55, C56, C57, C58, C59, C60, C61, C62, C63, C64, C65, 
+                                     C66, C67, C68, C69, C70, C71, C72, C73, C74, C75, C76, C77, C78, C79, C80, C81, 
+                                     C82, C83, C84, C85, C86, C87, C88, C89, C90, C91, C92, C93, C94, C95, C96, C97, 
+                                     C98, C99, C100, C101, C102, C103, C104, C105, C106, C107, C108]
+    
+    let Deck: string = prompt("Gib deine Kartenanzahl ein"); 
+    let x: number; 
+    for (let i : number = 0; i < x; i++) {
+       
+        let randomCards = Cards[Math.floor(Cards.length * Math.random())];
+    }
+    
+    function createcards(_color: string, _kartenwert: string) : void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        
+        let s: CSSStyleDeclaration = div.style;
+       
+ 
+
+
+}
