8f2ec1838b69386fa032a05b61cf0c20dbb2c1ff
Arno <Arno@X-Ray>
2018-11-08 19:42:16 +0100

    aufgabe 2 detailverbessserung

--- Aufgabe 2/UNOneu.ts
+++ Aufgabe 2/UNOneu.ts
@@ -7,76 +7,56 @@ Datum: 28.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
 
-namespace UNO {
+namespace Uno {
 
-    interface UNOKarten {
+    document.addEventListener("DOMContentLoaded", content);
+
+    interface Card {
         color: string;
-        kartenwert: string;
+        value: string;
     }
 
-
-    let alleKarten: UNOKarten[] = [
-        { color: "#ff0000", kartenwert: "0" }, { color: "#ff0000", kartenwert: "1" }, { color: "#ff0000", kartenwert: "1" }, { color: "#ff0000", kartenwert: "2" }, { color: "#ff0000", kartenwert: "2" }, { color: "#ff0000", kartenwert: "3" }, { color: "#ff0000", kartenwert: "3" }, { color: "#ff0000", kartenwert: "4" }, { color: "#ff0000", kartenwert: "4" }, { color: "#ff0000", kartenwert: "5" },
-        { color: "#ff0000", kartenwert: "5" }, { color: "#ff0000", kartenwert: "6" }, { color: "#ff0000", kartenwert: "6" }, { color: "#ff0000", kartenwert: "7" }, { color: "#ff0000", kartenwert: "7" }, { color: "#ff0000", kartenwert: "8" }, { color: "#ff0000", kartenwert: "8" }, { color: "#ff0000", kartenwert: "9" }, { color: "#ff0000", kartenwert: "9" }, { color: "#ff0000", kartenwert: "Stop" },
-        { color: "#ff0000", kartenwert: "Stop" }, { color: "#ff0000", kartenwert: "Swap" }, { color: "#ff0000", kartenwert: "Swap" }, { color: "#ff0000", kartenwert: "+2" }, { color: "#ff0000", kartenwert: "+2" }, { color: "#0000ff", kartenwert: "0" }, { color: "#0000ff", kartenwert: "1" }, { color: "#0000ff", kartenwert: "1" }, { color: "#0000ff", kartenwert: "2" }, { color: "#0000ff", kartenwert: "2" },
-        { color: "#0000ff", kartenwert: "3" }, { color: "#0000ff", kartenwert: "3" }, { color: "#0000ff", kartenwert: "4" }, { color: "#0000ff", kartenwert: "4" }, { color: "#0000ff", kartenwert: "5" }, { color: "#0000ff", kartenwert: "5" }, { color: "#0000ff", kartenwert: "6" }, { color: "#0000ff", kartenwert: "6" }, { color: "#0000ff", kartenwert: "7" }, { color: "#0000ff", kartenwert: "7" },
-        { color: "#0000ff", kartenwert: "8" }, { color: "#0000ff", kartenwert: "8" }, { color: "#0000ff", kartenwert: "9" }, { color: "#0000ff", kartenwert: "9" }, { color: "#0000ff", kartenwert: "Stop" }, { color: "#0000ff", kartenwert: "Stop" }, { color: "#0000ff", kartenwert: "Swap" }, { color: "#0000ff", kartenwert: "Swap" }, { color: "#0000ff", kartenwert: "+2" }, { color: "#0000ff", kartenwert: "+2" },
-        { color: "#00ff00", kartenwert: "0" }, { color: "#00ff00", kartenwert: "1" }, { color: "#00ff00", kartenwert: "1" }, { color: "#00ff00", kartenwert: "2" }, { color: "#00ff00", kartenwert: "2" }, { color: "#00ff00", kartenwert: "3" }, { color: "#00ff00", kartenwert: "3" }, { color: "#00ff00", kartenwert: "4" }, { color: "#00ff00", kartenwert: "4" }, { color: "#00ff00", kartenwert: "5" },
-        { color: "#00ff00", kartenwert: "5" }, { color: "#00ff00", kartenwert: "6" }, { color: "#00ff00", kartenwert: "6" }, { color: "#00ff00", kartenwert: "7" }, { color: "#00ff00", kartenwert: "7" }, { color: "#00ff00", kartenwert: "8" }, { color: "#00ff00", kartenwert: "8" }, { color: "#00ff00", kartenwert: "9" }, { color: "#00ff00", kartenwert: "9" }, { color: "#00ff00", kartenwert: "Stop" },
-        { color: "#00ff00", kartenwert: "Stop" }, { color: "#00ff00", kartenwert: "Swap" }, { color: "#00ff00", kartenwert: "Swap" }, { color: "#00ff00", kartenwert: "+2" }, { color: "#00ff00", kartenwert: "+2" },
-        { color: "#ffff00", kartenwert: "0" }, { color: "#ffff00", kartenwert: "1" }, { color: "#ffff00", kartenwert: "1" }, { color: "#ffff00", kartenwert: "2" }, { color: "#ffff00", kartenwert: "2" }, { color: "#ffff00", kartenwert: "3" }, { color: "#ffff00", kartenwert: "3" }, { color: "#ffff00", kartenwert: "4" }, { color: "#ffff00", kartenwert: "4" }, { color: "#ffff00", kartenwert: "5" },
-        { color: "#ffff00", kartenwert: "5" }, { color: "#ffff00", kartenwert: "6" }, { color: "#ffff00", kartenwert: "6" }, { color: "#ffff00", kartenwert: "7" }, { color: "#ffff00", kartenwert: "7" }, { color: "#ffff00", kartenwert: "8" }, { color: "#ffff00", kartenwert: "8" }, { color: "#ffff00", kartenwert: "9" }, { color: "#ffff00", kartenwert: "9" }, { color: "#ffff00", kartenwert: "Stop" },
-        { color: "#ffff00", kartenwert: "Stop" }, { color: "#ffff00", kartenwert: "Swap" }, { color: "#ffff00", kartenwert: "Swap" }, { color: "#ffff00", kartenwert: "+2" }, { color: "#ffff00", kartenwert: "+2" },
-        { color: "#black", kartenwert: "+4" }, { color: "#black", kartenwert: "+4" }, { color: "#black", kartenwert: "+4" }, { color: "#black", kartenwert: "+4" }];
-
+    let deck: Card[] = [
+        { color: "#ff0000", value: "0" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "2" }, { color: "#ff0000", value: "2" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "5" },
+        { color: "#ff0000", value: "5" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "Stop" },
+        { color: "#ff0000", value: "Stop" }, { color: "#ff0000", value: "Swap" }, { color: "#ff0000", value: "Swap" }, { color: "#ff0000", value: "+2" }, { color: "#ff0000", value: "+2" }, { color: "#0000ff", value: "0" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "2" },
+        { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "7" },
+        { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "Stop" }, { color: "#0000ff", value: "Stop" }, { color: "#0000ff", value: "Swap" }, { color: "#0000ff", value: "Swap" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "+2" },
+        { color: "#00ff00", value: "0" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "5" },
+        { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "Stop" },
+        { color: "#00ff00", value: "Stop" }, { color: "#00ff00", value: "Swap" }, { color: "#00ff00", value: "Swap" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "+2" },
+        { color: "#ffff00", value: "0" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "5" },
+        { color: "#ffff00", value: "5" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "Stop" },
+        { color: "#ffff00", value: "Stop" }, { color: "#ffff00", value: "Swap" }, { color: "#ffff00", value: "Swap" }, { color: "#ffff00", value: "+2" }, { color: "#ffff00", value: "+2" },
+        { color: "#black", value: "+4" }, { color: "#black", value: "+4" }, { color: "#black", value: "+4" }, { color: "#black", value: "+4" }];
 
 
     function random(): number {
-        return Math.floor(Math.random() * Math.floor(alleKarten.length));
+        return Math.floor(Math.random() * Math.floor(deck.length));
     }
 
 
-    function createCard(card: number): void {
+    function createCards(_cards: number): void {
 
-        for (let i: number = 0; i < card; i++) {
+        for (let i: number = 0; i < _cards; i++) {
             let index: number = random();
             let div: HTMLElement = document.createElement("div");
             div.setAttribute("class", "Einzelkarten");
-            div.innerText = alleKarten[index].kartenwert;
+            div.innerText = deck[index].value;
             let hand: HTMLElement = document.getElementById("Hand");
             hand.appendChild(div);
 
             let s: CSSStyleDeclaration = div.style;
-            s.backgroundColor = alleKarten[index].color;
+            s.backgroundColor = deck[index].color;
         }
     }
 
 
-    function ablage(): void {
-        let div: HTMLDivElement = document.createElement("div");
-        document.body.appendChild(div);
-        document.getElementById("Ablage").innerHTML += "Ablage";
-    }
-
-
-    function deck(): void {
-        let div: HTMLDivElement = document.createElement("div");
-        document.body.appendChild(div);
-        document.getElementById("Stapel").innerHTML += "Deck";
-    }
-
     function content(): void {
-        let gesamtKarten: number = (alleKarten.length);
-        let kartenWahl: string = prompt("Gib deine Kartenanzahl ein");
-        gesamtKarten = Number(kartenWahl);
+        let allCards: number = (deck.length);
+        let cardselection: string = prompt("Gib deine Kartenanzahl ein");
+        allCards = Number(cardselection);
 
-        createCard(gesamtKarten);
-
-        ablage();
-        deck();
+        createCards(allCards);
     }
-
-
-    document.addEventListener("DOMContentLoaded", content);
-
 }
