0a36e074dcc7a6bafab979e50e98ce71e12bb627
Arno <Arno@X-Ray.eduroam.hs-furtwangen.de>
2018-11-29 11:28:21 +0100

    a 5 halb fertig

--- Aufgabe 5/Aufgabe5.js
+++ Aufgabe 5/Aufgabe5.js
@@ -1,17 +1,19 @@
+/**
+Aufgabe: 4
+Name: Arno Lindner
+Matrikel: 259153
+Datum: 18.11.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+**/
 var Aufgabe5;
 (function (Aufgabe5) {
     window.addEventListener("load", init);
     function init(_event) {
-        //        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-        //
-        //        for (let i: number = 0; i < fieldsets.length; i++) {
-        //            let fieldset: HTMLFieldSetElement = fieldsets[i];
-        //            fieldset.addEventListener("change", cartChange);
-        getSteppers(Aufgabe5.data1);
-        getRadioBoxes(Aufgabe5.data2);
+        createFieldset(Aufgabe5.data);
+        document.addEventListener("change", handleChange);
     }
-    //let shoppingcart: Product[] = [];
-    function getSteppers(_product) {
+    function createFieldset(_product) {
         for (let key in _product) {
             let fieldset = document.createElement("fieldset");
             fieldset.setAttribute("id", key);
@@ -29,7 +31,10 @@ var Aufgabe5;
                 input.value = "0";
                 input.id = value[i].name;
                 input.setAttribute("group", key);
-                input.setAttribute("number", "" + i);
+                input.setAttribute("name", value[i].name);
+                input.setAttribute("price", value[i].price.toString() + " Euro");
+                input.setAttribute("value", "0");
+                input.addEventListener("change", handleChange);
                 let label = document.createElement("label");
                 label.innerHTML = " " + value[i].name + " " + value[i].price + " Euro";
                 input.appendChild(label);
@@ -37,33 +42,59 @@ var Aufgabe5;
                 fieldset.appendChild(label);
                 fieldset.appendChild(document.createElement("br"));
                 fieldset.appendChild(document.createElement("br"));
+                if (key == "Baumart" || key == "Baumspitzen" || key == "Post-Weg") {
+                    input.type = "radio";
+                    input.required = true;
+                    input.name = key;
+                    input.id = value[i].name;
+                    input.setAttribute("group", key);
+                    input.setAttribute("name", value[i].name);
+                    input.setAttribute("price", value[i].price.toString() + " Euro");
+                    input.setAttribute("value", "0");
+                    input.addEventListener("change", handleChange);
+                }
             }
         }
     }
-    function getRadioBoxes(_product) {
-        for (let key in _product) {
-            let fieldset = document.createElement("fieldset");
-            fieldset.setAttribute("id", key);
-            let legend = document.createElement("legend");
-            legend.innerText = key;
-            fieldset.appendChild(legend);
-            document.body.appendChild(fieldset);
-            let value = _product[key];
-            for (let i = 0; i < value.length; i++) {
-                let input = document.createElement("input");
-                input.type = "radio";
-                input.required = true;
-                input.name = key;
-                input.id = value[i].name;
-                input.setAttribute("group", key);
-                input.setAttribute("number", "" + i);
-                let label = document.createElement("label");
-                label.innerHTML = " " + value[i].name + " " + value[i].price + " Euro";
-                input.appendChild(label);
-                fieldset.appendChild(input);
-                fieldset.appendChild(label);
-                fieldset.appendChild(document.createElement("br"));
-                fieldset.appendChild(document.createElement("br"));
+    //    function createCart(_event: Event): void {
+    //       // let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+    //        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+    //        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+    //       
+    //        
+    //        for (let i: number = 0; i < inputs.length; i++) {
+    //            
+    //            if (inputs[i].checked) {
+    //                let productname: string = target.getAttribute("name");
+    //                let productprice: number = parseFloat(target.getAttribute("price"));
+    //                console.log(productname, " ", productprice);
+    //                
+    //            }
+    //            
+    //        }
+    //        /** schleifen und if abfragen durch alle fieldsets durchlaufen lassen und schauen ob es gecheckt ist oder nicht wenn ja zu 
+    //        HTMLInputElement.checked f�r radio buttons, f�r stepper aktueller wert der hochgez�hlt wird)**/
+    //    }
+    function handleChange(_event) {
+        let inputs = document.getElementsByTagName("input");
+        let target = _event.target;
+        let cart = document.getElementById("Cart");
+        let div = document.createElement("div");
+        cart.appendChild(div);
+        for (let i = 0; i < inputs.length; i++) {
+            let product = inputs[i];
+            if (product.type == "radio") {
+                if (product.checked == true) {
+                    let productname = target.getAttribute("name");
+                    let productprice = parseFloat(target.getAttribute("price"));
+                    div.innerHTML = productname + " " + productprice + " Euro";
+                }
+            }
+            if (product.type == "number") {
+                let productname = target.getAttribute("name");
+                let productprice = parseFloat(target.getAttribute("price"));
+                let amount = target.value;
+                div.innerHTML = amount + "X " + productname + " " + productprice + " Euro";
             }
         }
     }
--- Aufgabe 5/Aufgabe5.ts
+++ Aufgabe 5/Aufgabe5.ts
@@ -7,11 +7,8 @@ Datum: 18.11.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
 
-/** bekomme es leider nicht hin die Produkte in den shopping cart zu pushen mir machen hier die verschiednen typen schwierigkeiten 
-string und Product, ich weis aber leider nicht, wie man das anders lösen kann **/
-
 namespace Aufgabe5 {
-    
+
 
     window.addEventListener("load", init);
 
@@ -19,127 +16,168 @@ namespace Aufgabe5 {
 
     function init(_event: Event): void {
 
-        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-
-        for (let i: number = 0; i < fieldsets.length; i++) {
-            let fieldset: HTMLFieldSetElement = fieldsets[i];
-            fieldset.addEventListener("change", cartChange);
 
 
-            getSteppers(data1);
-            getRadioBoxes(data2);
-            createDatatFormular();
-
-        }
-
-        let shoppingcart: Product[] = [];
-
-        function getSteppers(_product: OptionArticles): void {
 
 
-            for (let key in _product) {
-                let fieldset: HTMLFieldSetElement = document.createElement("fieldset");
-                fieldset.setAttribute("id", key);
-                let legend: HTMLLegendElement = document.createElement("legend");
-                legend.innerText = key;
-                fieldset.appendChild(legend);
-                document.body.appendChild(fieldset);
-                let value: Product[] = _product[key];
-                for (let i: number = 0; i < value.length; i++) {
-                    let input: HTMLInputElement = document.createElement("input");
-                    input.type = "number";
-                    input.min = "0";
-                    input.max = "30";
-                    input.step = "1";
-                    input.value = "0";
-                    input.id = value[i].name;
-                    input.setAttribute("group", key);
-                    input.setAttribute("number", "" + i);
-                    let label: HTMLLabelElement = document.createElement("label");
-                    label.innerHTML = " " + value[i].name + " " + value[i].price + " Euro";
-                    input.appendChild(label);
-                    fieldset.appendChild(input);
-                    fieldset.appendChild(label);
-                    fieldset.appendChild(document.createElement("br"));
-                    fieldset.appendChild(document.createElement("br"));
-                }
-            }
-        }
+        createFieldset(data);
+        document.addEventListener("change", handleChange);
 
-        function getRadioBoxes(_product: OneOptionArticles): void {
 
-            for (let key in _product) {
-                let fieldset: HTMLFieldSetElement = document.createElement("fieldset");
-                fieldset.setAttribute("id", key);
-                let legend: HTMLLegendElement = document.createElement("legend");
-                legend.innerText = key;
-                fieldset.appendChild(legend);
-                document.body.appendChild(fieldset);
-                let value: Product[] = _product[key];
-                for (let i: number = 0; i < value.length; i++) {
-                    let input: HTMLInputElement = document.createElement("input");
+    }
+
+
+    function createFieldset(_product: OptionArticles): void {
+
+
+        for (let key in _product) {
+            let fieldset: HTMLFieldSetElement = document.createElement("fieldset");
+            fieldset.setAttribute("id", key);
+            let legend: HTMLLegendElement = document.createElement("legend");
+            legend.innerText = key;
+            fieldset.appendChild(legend);
+            document.body.appendChild(fieldset);
+            let value: Product[] = _product[key];
+            for (let i: number = 0; i < value.length; i++) {
+                let input: HTMLInputElement = document.createElement("input");
+                input.type = "number";
+                input.min = "0";
+                input.max = "30";
+                input.step = "1";
+                input.value = "0";
+                input.id = value[i].name;
+                input.setAttribute("group", key);
+                input.setAttribute("name", value[i].name);
+                input.setAttribute("price", value[i].price.toString() + " Euro");
+                input.setAttribute("value", "0");
+                input.addEventListener("change", handleChange);
+                let label: HTMLLabelElement = document.createElement("label");
+                label.innerHTML = " " + value[i].name + " " + value[i].price + " Euro";
+                input.appendChild(label);
+                fieldset.appendChild(input);
+                fieldset.appendChild(label);
+                fieldset.appendChild(document.createElement("br"));
+                fieldset.appendChild(document.createElement("br"));
+
+                if (key == "Baumart" || key == "Baumspitzen" || key == "Post-Weg") {
+
                     input.type = "radio";
                     input.required = true;
                     input.name = key;
                     input.id = value[i].name;
                     input.setAttribute("group", key);
-                    input.setAttribute("number", "" + i);
-                    let label: HTMLLabelElement = document.createElement("label");
-                    label.innerHTML = " " + value[i].name + " " + value[i].price + " Euro";
-                    input.appendChild(label);
-                    fieldset.appendChild(input);
-                    fieldset.appendChild(label);
-                    fieldset.appendChild(document.createElement("br"));
-                    fieldset.appendChild(document.createElement("br"));
+                    input.setAttribute("name", value[i].name);
+                    input.setAttribute("price", value[i].price.toString() + " Euro");
+                    input.setAttribute("value", "0");
+                    input.addEventListener("change", handleChange);
+
                 }
             }
         }
-
-        function createDatatFormular(): void {
-
-            let input: HTMLInputElement = document.createElement("input");
-            input.setAttribute("group", "data");
-            let data: HTMLElement = document.getElementById("Data");
-            data.appendChild(input);
-        }
-
-        function cartChange(_event: Event): void {
-
-            let target: HTMLInputElement = <HTMLInputElement>_event.target;
-            let productgroup: string = target.getAttribute("group");
-            let productnumber: number = parseInt(target.getAttribute("number"));
-            let group: string;
-
-            switch (productgroup) {
-
-                case "Kerzen": group = "Kugeln"; break;
-
-                case "Lichterketten": group = "Lichterketten"; break;
-
-                case "Kerzen": group = "Kerzen"; break;
-
-                case "Baumart": group = "Baumart"; break;
-
-                case "Baumspitzen": group = "Baumspitzen"; break;
-
-                case "Post-Weg": group = "Post-Weg"; break;
-
-            }
-
-
-            shoppingcart.push(group[productnumber]);
-            displayShoppingCart();
-        }
-
-        function displayShoppingCart(): void {
-            let div: HTMLElement = document.createElement("div");
-
-            for (let x: number = 0; x < shoppingcart.length; x++) {
-                let shoppingcartfieldset: HTMLElement = document.getElementById("shoppingcart");
-                shoppingcartfieldset.appendChild(div);
-                shoppingcartfieldset.innerHTML = "";
-                div.innerHTML = shoppingcart[x].name + " " + shoppingcart[x].price + " Euro";
-            }
-        }
     }
-}   
+
+    //    function createCart(_event: Event): void {
+    //       // let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+    //        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+    //        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+    //       
+    //        
+    //        for (let i: number = 0; i < inputs.length; i++) {
+    //            
+    //            if (inputs[i].checked) {
+    //                let productname: string = target.getAttribute("name");
+    //                let productprice: number = parseFloat(target.getAttribute("price"));
+    //                console.log(productname, " ", productprice);
+    //                
+    //            }
+    //            
+    //        }
+    //        /** schleifen und if abfragen durch alle fieldsets durchlaufen lassen und schauen ob es gecheckt ist oder nicht wenn ja zu 
+    //        HTMLInputElement.checked für radio buttons, für stepper aktueller wert der hochgezählt wird)**/
+    //    }
+
+
+
+    function handleChange(_event: Event): void {
+
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+        let cart: HTMLElement = document.getElementById("Cart");
+        let div: HTMLElement = document.createElement("div");
+        cart.appendChild(div);
+
+
+
+
+
+
+        for (let i: number = 0; i < inputs.length; i++) {
+            let product: HTMLInputElement = inputs[i];
+
+
+            if (product.type == "radio") {
+
+                if (product.checked == true) {
+
+                    let productname: string = target.getAttribute("name");
+                    let productprice: number = parseFloat(target.getAttribute("price"));
+                    div.innerHTML = productname + " " + productprice + " Euro";
+                }
+
+                //                else if (product.checked == false) {
+                //                    displayCart(_event);
+                //
+                //                }
+            }
+
+            if (product.type == "number") {
+                let productname: string = target.getAttribute("name");
+                let productprice: number = parseFloat(target.getAttribute("price"));
+                let amount: string = target.value;
+                div.innerHTML = amount + "X " + productname + " " + productprice + " Euro";
+
+               
+
+
+
+
+            }
+        }
+
+    }
+
+//
+//    function displayCart(_event: Event): void {
+//        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+//        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+//        let newcart: HTMLElement = document.getElementById("Cart");
+//        let div: HTMLElement = document.createElement("div");
+//        newcart.appendChild(div);
+//        
+//
+//        newcart.appendChild(div);
+//
+//
+//        for (let x: number = 0; x < inputs.length; x++) {
+//            let product: HTMLInputElement = inputs[x];
+//            let amount: number = parseFloat(inputs[x].value);
+//
+//
+//            if (product.type == "number") {
+//
+//                if (amount < 1) {
+//                    div.innerHTML = "";
+//                    let productname: string = target.getAttribute("name");
+//                    let productprice: number = parseFloat(target.getAttribute("price"));
+//                    let amount: string = target.value;
+//                    div.innerHTML = amount + "X " + productname + " " + productprice + " Euro";
+//
+//
+//                }
+//            }
+//
+//
+//
+//        }
+//    }
+}
