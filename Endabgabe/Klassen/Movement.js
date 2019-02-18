6223c3ad84b2d05ca48da873b689bfcb71cf09a9
Arno <Arno@X-Ray.fritz.box>
2019-02-18 15:31:45 +0100

    endabgabe

---
+++ Endabgabe/Klassen/Movement.js
@@ -0,0 +1,11 @@
+var Endabgabe;
+(function (Endabgabe) {
+    class Movement extends Endabgabe.ObjectStructure {
+        move() { }
+        ;
+        draw() { }
+        ;
+    }
+    Endabgabe.Movement = Movement;
+})(Endabgabe || (Endabgabe = {}));
+//# sourceMappingURL=Movement.js.map
---
+++ Endabgabe/Klassen/Movement.ts
@@ -0,0 +1,10 @@
+namespace Endabgabe {
+
+    export class Movement extends ObjectStructure {
+
+        speed: number;
+
+        move(): void { };
+        draw(): void { };
+    }
+}
